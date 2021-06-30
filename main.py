7cdd41651a34c31577e314369156f8a2c07c1993
pintermor9 <pintermor9@gmail.com>
2021-06-30 14:37:05 +0200

    heroku

--- main.py
+++ main.py
@@ -3,20 +3,14 @@ from discord.ext import commands, tasks
 import os
 import yaml
 from itertools import cycle
-from keep_alive import keep_alive
 
 with open(r'./data/settings.yml') as file:
-	settings = yaml.full_load(file)
-	print(settings)
+    settings = yaml.full_load(file)
+    print(settings)
 
 #General settings
 prefix = settings['prefix']
-if settings['TOKEN'] == "os.getenv('TOKEN')" or settings[
-        'TOKEN'] == 'os.getenv("TOKEN")':
-	TOKEN = os.getenv('TOKEN')
-else:
-	TOKEN = settings['TOKEN']
-webServer = settings['webServer']
+TOKEN = settings['TOKEN']
 owner_IDs = settings['owner_IDs']
 
 #Downtime settings:
@@ -44,43 +38,40 @@ client = commands.Bot(command_prefix=prefix,
                       intents=intents)
 
 if downAnnouncement:
-	cycleActivities = False
-	noCycleActivity = downAnnouncementStatus
+    cycleActivities = False
+    noCycleActivity = downAnnouncementStatus
 
 
 def is_owner(ctx):
-	return ctx.message.author.id in owner_IDs
+    return ctx.message.author.id in owner_IDs
 
 
 @client.event
 async def on_connect():
-	print('Connected to discord.')
-	print('Current discord.py version: {.__version__}'.format(discord))
+    print('Connected to discord.')
+    print('Current discord.py version: {.__version__}'.format(discord))
 
 
 @client.event
 async def on_ready():
-	print('Logged in as {0.user}!'.format(client))
-	await client.change_presence(
-	    status=discord.Status.idle,
-	    activity=discord.Game("Initializing... Please wait."))
+    print('Logged in as {0.user}!'.format(client))
+    await client.change_presence(
+        status=discord.Status.idle,
+        activity=discord.Game("Initializing... Please wait."))
 
-	if webServer:
-		keep_alive()
-
-	if cycleActivities:
-		activityLoop.start()
-	else:
-		await client.change_presence(activity=discord.Game(noCycleActivity))
+    if cycleActivities:
+        activityLoop.start()
+    else:
+        await client.change_presence(activity=discord.Game(noCycleActivity))
 
 
 @tasks.loop(seconds=10)
 async def activityLoop():
-	await client.change_presence(activity=discord.Game(next(loopActivities)))
+    await client.change_presence(activity=discord.Game(next(loopActivities)))
 
 
 for file in os.listdir('./cogs'):
-	if file.endswith('.py'):
-		client.load_extension(f'cogs.{file[:-3]}')
+    if file.endswith('.py'):
+        client.load_extension(f'cogs.{file[:-3]}')
 
 client.run(TOKEN)
